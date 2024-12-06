7d54164cf87d98881bdd163c8687f1d206a6a54a
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-12-06 10:29:41 +0900

    app.bladeを編集 Font Awesomeを導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Bootstrap -->
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+         <!-- Font Awesome -->
+         <script src="https://kit.fontawesome.com/572b6cd5b9.js" crossorigin="anonymous"></script>
     </head>
     <body>
         <div id="app">
