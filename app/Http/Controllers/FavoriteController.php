0ca208b6d55dc7d77d014ac679fe8ed4a4d000ec
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-12-08 00:10:31 +0900

    show.bladeを編集 if文でお気に入りを解除とお気に入りを設定。 if文を使いfacorite_productsがある削除し、 ない場合に登録に切り替える

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
-    //
+    public function store($product_id)
+    {   //認証＆リレーションシップの操作 & 中間テーブルにレコードを挿入
+        Auth::user()->favorite_products()->attach($product_id);
+
+        return back();
+    }
+
+    public function destroy($product_id)
+    {
+        Auth::user()->favorite_products()->detach($product_id);
+
+        return back();
+    }
 }
