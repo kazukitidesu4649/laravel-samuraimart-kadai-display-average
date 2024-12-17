ed4009d1c006ab73c8671e4a03ca4e8c5537f1ed
kazukitidesu4649 <kazukitidesu@gmail.com>
2024-12-17 10:32:44 +0900

    新着商品を４件トップページに表示する機能を実装

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
+< class="row">
     <div class="col-2">
         @component('components.sidebar', ['categories' => $categories, 'major_categories' => $major_categories])
         @endcomponent
     </div>
-    <div class="col-9">
+    < class="col-9">
       <h1>おすすめ商品</h1>
       <div class="row"> 
         <div class="col-4">
@@ -54,64 +54,24 @@
           <h1>新着商品</h1>
           <a href="{{ route('products.index', ['sort'=>'id', 'direction'=>'desc']) }}">もっと見る</a>
         </div>
-        <div class="row">
-            <div class="col-3">
-                  <a href="#">
-                      <img src="{{ asset('img/robot-vacuum-cleaner.jpg') }}" class="img-thumbnail">
-                  </a>
-                  <div class="row">
-                      <div class="col-12">
-                          <p class="samuraimart-product-label mt-2">
-                              ロボット掃除機<br>
-                              <label> ¥55000</label>
-                          </p>
-                      </div>
+        @foreach ($recentry_products as $recently_product)
+          <div class="col-3">
+              <a href="{{ route('products.show', $recentry_product) }}">
+                  @if ($recently_product->image !== '')
+                      <img src="{{ asset($recently_product->image) }}" class="img-thumbnail">
+                  @else
+                      <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+                  @endif
+              </a>
+              <div class="row">
+                  <div class="col-12">
+                      <p class="samuraimart-product-label mt-2">
+                          {{ $recentry_product->name }}<br>
+                          <label>¥{{ $recentry_product->price }}</label>
+                      </p>
                   </div>
-            </div>
-
-            <div class="col-3">
-                <a href="#">
-                    <img src="{{ asset('img/sofa.jpg')}}" class="img-thumbnail">
-                </a>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="samuraimart-product-label mt-2">
-                            3人掛けソファー<br>
-                            <label>¥35000</label>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-3">
-                <a href="#">
-                    <img src="{{ asset('img/cup.jpg')}}" class="img-thumbnail">
-                </a>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="samuraimart-product-label mt-2">
-                            コーヒーカップ<br>
-                            <label>¥1000</label>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-3">
-                <a href="#">
-                    <img src="{{ asset('img/cutlery.jpg')}}" class="img-thumbnail">
-                </a>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="samuraimart-product-label mt-2">
-                            食器カトラリーセット<br>
-                            <label>¥2000</label>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
+              </div>
+          </div>
+        @endforeach
 </div>
 @endsection
